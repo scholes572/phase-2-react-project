7b8f88e3be7b1570143f44d3b7b4d342c02e1977
Agostino <agostinoscholes572@gmail.com>
2025-07-20 21:27:33 +0300

    made changes in the TaskList.jsx

--- frontend/src/TaskList.jsx
+++ frontend/src/TaskList.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState }from"react";
-function TaskList () {
-    const [tasks, setTasks]=useState([]);
+import { useEffect, useState } from "react";
+
+function TaskList ({tasks: propTasks, onToggleTask}) {
+    const [tasks, setTasks] = useState([]);
     useEffect(() => {
         fetch("http://localhost:3000/tasks")
         .then((r) => r.json())
@@ -8,19 +9,26 @@ function TaskList () {
 
     }, [])
     return (
-        <div>
-            <h2>
-               Tasks
-            </h2>
-            <ul>
-                {tasks.map((task) => (
-                    <li key={task.id}>
-                        {task.title} {task.completed ? "(Done)" : "(Pending)"}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+    <div>
+      <h2>Tasks</h2>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <span
+              onClick={() => onToggleTask(task.id, !task.completed)}
+              style={{
+                cursor: "pointer",
+                textDecoration: task.completed ? "line-through" : "none",
+              }}
+            >
+              {task.title}
+            </span>{" "}
+            {task.completed ? "(Done)" : "(Pending)"}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default TaskList;
