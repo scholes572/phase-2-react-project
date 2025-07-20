5928140a979a4e79ed178a1a1e658245c4d0e800
Agostino <agostinoscholes572@gmail.com>
2025-07-20 23:27:54 +0300

    made changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,16 +16,30 @@ function App() {
     setTasks((prevTasks) => [...prevTasks, newTask]);
   };
 
+  const handleToggleTask = (id, newCompletedStatus) => {
+    fetch(`http://localhost:3000/tasks/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ completed: newCompletedStatus }),
+    })
+      .then((res) => res.json())
+      .then((updatedTask) => {
+        const updatedTasks = tasks.map((task) =>
+          task.id === updatedTask.id ? updatedTask : task
+        );
+        setTasks(updatedTasks);
+      });
+  };
+
   return (
     <div className="App">
       <h1>To-Do Manager</h1>
       <TaskForm onAddTask={handleAddTask} />
-      <TaskList tasks={tasks} />
+      <TaskList tasks={tasks} onToggleTask={handleToggleTask} />
     </div>
   );
 }
-
-
- 
 
 export default App;
